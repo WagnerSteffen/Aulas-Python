f9a776e37936eb25b736925eb812ddf6e882f568
Wagner Steffen <wagnersteffen25@gmail.com>
2023-03-14 16:36:03 -0300

    rock paper cisors final

--- mini_projects/rock_paper_cisors.py
+++ mini_projects/rock_paper_cisors.py
@@ -1,16 +1,18 @@
 import random
 print("Hello, this is a game of Rock, Paper and Cisors!")
 options = ("Rock", "Paper", "Cisors")
-
+count = 0
 def main():
+    global count
     #show game
     while True:
         c_choice = computer_choice()
         p_choice = player_choice()
         print(f"\nYou choose {p_choice} and the computer choose {c_choice}\n")
-        who_wins = determine_winner(p_choice, c_choice)
+        determine_winner(p_choice, c_choice)
         keep_playing = str(input("\nDo you wanna play again?\n")).lower()
         if not keep_playing.startswith("y"):
+            print(f"You win {count} times")
             break
 def player_choice():
     print("\n")
@@ -23,6 +25,8 @@ def computer_choice(y = options):
     return random.choice(y)
 
 def determine_winner(player_choice, computer_choice):
+    # sourcery skip: extract-duplicate-method, remove-unnecessary-else
+    global count
     if player_choice == computer_choice:
         return print("It's a tie!")
 
@@ -30,19 +34,21 @@ def determine_winner(player_choice, computer_choice):
         if computer_choice == "Paper":
             return print("Computer wins")
         else:
+            count += 1
             return print("You win!")
 
     if player_choice == "Paper":
         if computer_choice == "Cisors":
             return print("Computer wins")
         else:
+            count += 1
             return print("You win!")
 
     if player_choice == "Cisors":
         if computer_choice == "Rock":
             return print("Computer wins")
         else:
+            count += 1
             return print("You win!")
 
 main()
-print(f"You win {count} times")
