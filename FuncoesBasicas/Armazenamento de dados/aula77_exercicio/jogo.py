b2f387ef62efdb40cd7c137ee12f18a773ec13ef
Wagner Steffen <wagnersteffen25@gmail.com>
2023-03-27 20:40:02 -0300

    progress in class 77 exercise

--- FuncoesBasicas/Armazenamento de dados/aula77_exercicio/jogo.py
+++ FuncoesBasicas/Armazenamento de dados/aula77_exercicio/jogo.py
@@ -40,17 +40,22 @@ LançaDeGelo --> max((For - Def/5)*random(0,1), 1) consumo de SP 10
 Cura --> recupera 10 consumo de SP 10
 """
 import dbm
+import random
 import datetime
+import luta
 
 class player():
-    def __init__(self, name, strength = 20, defense = 20, hp = 500, sp = 100) -> None:
+    def __init__(self, name: str, strength=20, defense=20, hp=500, sp=100, attacks=None) -> None:
+        if attacks is None:
+            attacks = ['Espadada', 'Flexada', 'Cura', 'Lança de Gelo']
         self._name = name
         self._strength = strength
         self._defense = defense
         self._hp = hp
         self._sp = sp
-        self._save = datetime.datetime.now()
+        # self._save = datetime.datetime.now()
         self._enemiesDefeated = 0
+        self.attacks = attacks
     def level_up(self, attribute):
         if attribute == 'strength':
             self._strenght += 5
@@ -63,15 +68,13 @@ class player():
         else:
             print("Attribute not supported")
 def main():
+    enemies_list = enemies()
     memory_card = dbm.open('saves.db', 'c')
     game = welcome_menu(memory_card)
-    
-    ogre = player("Ogre", 30, 5, 100, 5)
-    goblin = player("Goblin", 15, 10, 70, 10)
-    eskeleton = player("Eskeleton", 10, 20, 90, 20)
-    witch = player("Witch", 10, 30, 80, 20)
-    enemies = [ogre, goblin, eskeleton, witch]
-    
+    while game:
+        enemies_in_wave = list(map(lambda wave: 1 if wave < 10 else wave // 10, [player._enemiesDefeated]))
+        enemies_to_battle = [random.choice(enemies_list) for _ in enemies_in_wave[0]]
+
 def welcome_menu(memory):
     print(
     """
@@ -108,7 +111,15 @@ def player_menu():
     3 - GIVE UP\n"""
         )
     )
+def enemies():
+    ogre = player("Ogre", 30, 5, 100, 5, ['Clavada'])
+    goblin = player("Goblin", 15, 10, 70, 10, ['Flexada'])
+    eskeleton = player("Eskeleton", 10, 20, 90, 20, ['Espadada', 'Cura'])
+    witch = player("Witch", 10, 30, 80, 20, ['Relampago', 'Bola de Fogo', 'Espadada', 'Cura'])
+    return [ogre, goblin, eskeleton, witch]
 
+def enemies_in_wave(wave):
+    return 1 if wave < 10 else wave // 10
 
 def con():
     pass
--- FuncoesBasicas/Armazenamento de dados/aula77_exercicio/luta.py
+++ FuncoesBasicas/Armazenamento de dados/aula77_exercicio/luta.py
@@ -1,1 +1,15 @@
 import random
+"""
+Espada --> max((For - Def)*random(0,1), 1) consumo de SP 0
+Flexada --> max((For - Def/3)*random(0,1), 1) consumo de SP 2
+Clavada --> max((For - Def/1)*random(0,1), 1) consumo de SP 0
+Relampago --> max((For - Def/5)*random(0,1), 1) consumo de SP 5
+BolaDeFogo --> max((For - Def/5)*random(0,1), 1) consumo de SP 10
+LançaDeGelo --> max((For - Def/5)*random(0,1), 1) consumo de SP 10
+Cura --> recupera 10 consumo de SP 10
+"""
+
+class round():
+    def __init__(self, player, enemies):
+        pass
+    
